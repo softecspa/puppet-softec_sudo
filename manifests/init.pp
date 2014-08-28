d7604287ffeda364ec1b6c4427e6eccd0a55e491
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-08-28 15:25:17 +0000

    fix

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,12 +8,6 @@ class softec_sudo inherits sudo {
             source  => undef,
             content => template("softec_private/etc/sudoers_hardy.erb")
           }
-          file { '/etc/sudoers.d':
-            ensure  => directory,
-            mode   => '755',
-            owner => root,
-            group => root,
-          }
         }
         default : {
           File[$sudo::config_file] {
